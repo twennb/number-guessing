14087b26c6cdbc19608fcf3b7859ce55900646f4
twennb <tom.wennberg@hotmail.com>
2025-03-13 09:05:42 +0100

    initial program structure and plan

---
+++ number guessing app/number.py
@@ -0,0 +1,39 @@
+"""A CLI based python app where the user guesses a chosen number"""
+# plan:
+# 1. pick number 1-100
+# 2. prompt user to guess number
+# 3. if correct - user wins! - restart
+# 4. if wrong - give user clue higher/lower
+# 5. repeat from 2.
+
+
+def generate_number():
+    """generates a random number from 1-100"""
+    # code
+
+
+def user_prompt():
+    """prompts the user to guess a number"""
+    # code
+
+
+def higher_lower():
+    """gives the user a higher or lower hint depending on guess"""
+    # code
+
+
+def win():
+    """congratulates the user if they guess correctly"""
+    # code
+
+
+def main():
+    """the main function"""
+    # generate number
+    # prompt user
+    # evaluate guess
+    # reply with hint or congratulations
+
+
+if __name__ == "__main__":
+    main()
